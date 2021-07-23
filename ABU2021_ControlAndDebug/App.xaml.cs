f09c0f7ae95000ff539947dd35063d23764bca4a
Horie <makizushi2000.t.h@gmail.com>
2021-07-23 17:32:08 +0900

    Fix multiple launch
    多重起動しないようにした

--- ABU2021_ControlAndDebug/App.xaml.cs
+++ ABU2021_ControlAndDebug/App.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -10,8 +13,50 @@ namespace ABU2021_ControlAndDebug
 {
     /// <summary>
     /// App.xaml の相互作用ロジック
+    /// 
+    /// 
+    /// 多重起動の防止はここ
+    /// https://aibax.hatenadiary.org/entry/20080711/1215787534
     /// </summary>
     public partial class App : Application
     {
+        /*
+         * Win32APIをP/Invokeで使用するための宣言
+         */
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+        private static extern int ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private const int SW_HIDE           = 0;
+        private const int SW_SHOWNORMAL     = 1;
+        private const int SW_SHOWMINIMIZED  = 2;
+        private const int SW_SHOWMAXIMIZED  = 3;
+        private const int SW_SHOWNOACTIVATE = 4;
+        private const int SW_SHOW           = 5;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            /* 現在のプロセスを取得 */
+            Process currentProcess = Process.GetCurrentProcess();
+
+            /* 現在のプロセスと同名のプロセスを取得 */
+            Process[] processes = Process.GetProcessesByName(currentProcess.ProcessName);
+
+            foreach (Process p in processes)
+            {
+                /* 同名の他のプロセスがあれば... */
+                if (p.Id != currentProcess.Id)
+                {
+                    /* ウィンドウを全面に表示する */
+                    ShowWindow(p.MainWindowHandle, SW_SHOWMAXIMIZED);
+                    SetForegroundWindow(p.MainWindowHandle);
+
+                    /* 起動を中止してプログラムを終了 */
+                    this.Shutdown();
+                }
+            }
+        }
     }
 }
---
+++ ABU2021_ControlAndDebug/Models/ControlMachineBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MVVMLib;
+
+namespace ABU2021_ControlAndDebug.Models
+{
+    abstract class ControlMachineBase : NotifyPropertyChanged
+    {
+        #region Property
+        #region private
+
+        private bool _isEnabled;
+        private bool _isConnected;
+        private bool _isEmergencyStopped;
+
+        private Vector _centerPosition = new Vector();
+        private double _centerPositionRot;
+        #endregion
+
+        #region public
+        public bool IsEnabaled
+        {
+            get => _isEnabled;
+            set { SetProperty(ref _isEnabled, value); }
+        }
+        public bool IsConnected
+        {
+            get => _isConnected;
+            set { SetProperty(ref _isConnected, value); }
+        }
+
+        #region Get only
+        public bool IsEmergencyStopped
+        {
+            get => _isEmergencyStopped;
+            private set { SetProperty(ref _isEmergencyStopped, value); }
+        }
+        public Vector Positon
+        {
+            get => _centerPosition;
+            protected set { SetProperty(ref _centerPosition, value); }
+        }
+        public double PositonRot
+        {
+            get => _centerPositionRot;
+            protected set { SetProperty(ref _centerPositionRot, value); }
+        }
+        #endregion
+        #endregion
+        #endregion
+
+
+        #region Method
+        protected abstract void SaveDebugLog(string fileName);
+        #endregion
+    }
+}
